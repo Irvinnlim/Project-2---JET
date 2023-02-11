b1ac5381aaa8038bad42537fe685f546bacd3c40
irvinnlim <irvinnlim@gmail.com>
2023-02-11 15:38:36 +0800

    upload src folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Portfolio from "./Components/SubPage/Portfolio/Portfolio";
+import News from "./Components/SubPage/News/News";
+import Account from "./Components/SubPage/Account/Account";
+import StockEngine from "./Components/SubPage/StockPage/StockEngine";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <Header />
+      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stocks" element={<StockEngine />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </main>
     </div>
   );
 }
